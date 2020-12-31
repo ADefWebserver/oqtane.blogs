eedb814339a9b580e2c3ccea63a9d9813671b9aa
Michael Washington <webmaster@adefwebserver.com>
2020-12-30 19:04:11 -0800

    Implement IServerStartup

---
+++ Server/Startup/ServerStartup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using Oqtane.Infrastructure;
+using WilderMinds.MetaWeblog;
+
+namespace Oqtane.Blogs.Server.Startup
+{
+    public class ServerStartup : IServerStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMetaWeblog("/MetaWeblog");
+        }
+
+        public void ConfigureMvc(IMvcBuilder mvcBuilder)
+        {
+        }
+    }
+}
